bf4b36984e31051a16016f2da5f85ef0b1b4ebd4
mgholden <mgholden27@gmail.com>
2022-02-06 20:31:17 -0500

    Started Backend, Set DB, models, and Server

---
+++ models/users.js
@@ -0,0 +1,11 @@
+const mongoose = require("../db/connection");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  allergyOrIntolerance: [{ type: String }],
+  //^we can make an array of allergies or intolerances
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
