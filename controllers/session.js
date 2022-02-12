68115ddb01cf778e0732be299f3c606e9205327f
mgholden <mgholden27@gmail.com>
2022-02-12 18:42:47 -0500

    Created Empty Controller

---
+++ controllers/session.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const User = require('../models/users')
+const router = express.Router()
+
--- models/users.js
+++ models/users.js
@@ -3,6 +3,7 @@ const mongoose = require("../db/connection");
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
+  confirmPassword: { type: String, required: true},
   allergyOrIntolerance: [{ type: String }],
   //^we can make an array of allergies or intolerances
 });
