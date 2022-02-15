2df958cd9af73bf91a5349eb3dec25ab5dc0c8fa
veenaprakash123 <veenaprakash2018@gmail.com>
2022-02-15 18:10:54 -0500

    Routes for login functioning properly

--- db/connection.js
+++ db/connection.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose");
 
 const mongoURI = "mongodb://127.0.0.1:27017/entries";
 
+// Need to change the above to: 
+// const MONGODB_URI = process.env.MONGODB_URI
+
+// You'd also need to change below to:
+// mongoose.connect(MONGO_URI, {...})
+
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
--- server.js
+++ server.js
@@ -1,15 +1,26 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const PORT = 7200
+const SESSION_SECRET= process.env.SESSION_SECRET
 const cors = require('cors')
+const session = require('express-session')
 const dailyLogController = require('./controllers/dailyLog')
+const sessionController = require('./controllers/session')
 
 app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+
 app.use('/entries', dailyLogController)
+app.use('/session', sessionController)
 
 app.listen(PORT, () => {
     console.log('Allergy Busters is running on port', PORT, '🤧✅')
